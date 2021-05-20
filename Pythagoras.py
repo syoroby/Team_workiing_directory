95e0e8440df3ea7b60db99cef7e4005a3d9903f3
jjPark <lucero23@naver.com>
2021-05-20 14:26:35 +0900

    modified Pythagoras

--- Pythagoras.py
+++ Pythagoras.py
@@ -1,15 +1,18 @@
 ##피타고라스 공식##
 import math
 
-num1=input("x값 입력:")
-num2=input("y값 입력:")
-x=float(num1)
-y=float(num2)
+print("[[빗변의 길이 구하기]]")
+
+x=float(input("x값 입력:"))
+y=float(input("y값 입력:"))
+
 z=(x*x)+(y*y)
 
-print("z변의 길이는 ->",math.sqrt(z))
+print("z변의 길이는 ->",math.sqrt(z))  #빗변의 길이 출력
 
 ##피타고라스 삼각_리스트 활용##
+print("[[피타고라스 정리를 만족하는 조합]]")
+
 new_list=[]
 for x in range(1,30):
     for y in range(x,30):
@@ -17,20 +20,26 @@ for x in range(1,30):
             if x**2 + y**2 == z**2:
                 new_list.append((x,y,z))
                 
-print(new_list)
+for i in range(0,len(new_list)):    #엔터로 구분하여 가독성 향상
+               print(new_list[i])
 
 ##피타고라스 정리를 이용한, 두 점 사이의 거리 구하기##
 
-import math  class Point2D:
+print("[[두 점 사이의 거리 구하기]]")
+
+class Point2D:
     def __init__(self, x, y):
         self.x = x
         self.y = y
 
-p1 = Point2D(x=30, y=20)    # 점1
-p2 = Point2D(x=60, y=50)    # 점2
+t1, t2 = map(float, input("첫번째 점의 좌표를 입력하세요 (x, y) >> ").split(",")) #좌표를 입력받는 형식으로 수정
+t3, t4 = map(float, input("두번째 점의 좌표를 입력하세요 (x, y) >> ").split(","))
 
+p1 = Point2D(t1, t2)    # 점1
+p2 = Point2D(t3, t4)   # 점2
+ 
 a = p2.x - p1.x    # 선 a의 길이
 b = p2.y - p1.y    # 선 b의 길이
-
+ 
 c = math.sqrt((a * a) + (b * b))    # (a * a) + (b * b)의 제곱근을 구함
-print(c)    # 42.42640687119285
+print("두 점의 거리는 %d 입니다."%c)   
