829a9eeb2c2836822a3e46681bb02d7ae182cf5e
ParkJinWoo <syoroby@gmail.com>
2021-06-02 15:39:31 +0900

    지수함수_그래프.py 수정

--- 지수함수_그래프.py
+++ 지수함수_그래프.py
@@ -2,7 +2,13 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
-t = np.arange(0., 5., 0.2)
-line = plt.plot(t, t**2)
-plt.setp(line, color='r', linewidth=3.0)
-plt.show()
+def jisu(start_x, end_x, under):
+    x = np.arange(start_x, end_x, (end_x-start_x)/100)
+    line = plt.plot(x, under**x)
+    plt.setp(line, color='r', linewidth=3.0)
+    plt.show()
+
+x1 = float(input('x좌표의 시작점을 입력해주세요: '))
+x2 = float(input('x좌표의 끝점을 입력해주세요: '))
+under_ = float(input('밑을 입력해주세요: '))
+jisu(x1,x2,under_)
