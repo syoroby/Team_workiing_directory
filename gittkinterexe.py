c8cb74f7845c57e2b18d5b6cb53eccf04599a1a0
jjPark <lucero23@naver.com>
2021-06-09 02:18:34 +0900

    Toplevel

--- gittkinterexe.py
+++ gittkinterexe.py
@@ -1,5 +1,41 @@
 from tkinter import *
 
+def btn5():
+    win1 = Toplevel(window)
+    win1.configure(background = 'white')
+
+    title = Label(win1, text = "두 점 사이의 거리",
+                  font = ("맑은 고딕", "21", 'bold'),
+                  height=2,
+                  foreground = '#087666',
+                  background = 'white')
+    title.pack()
+
+    f1 = Frame(win1, bg = 'white')
+    f1.pack()
+
+    txt1 = Label(f1, text = "첫번째 점",
+                 background = 'white')
+    txt2 = Label(f1, text = "두번째 점",
+                 background = 'white')
+    txt3 = Label(f1, text = "좌표 (x, y)",
+                 background = 'white')
+
+    txt1.grid(row = 1, column = 0)
+    txt2.grid(row = 2, column = 0)
+    txt3.grid(row = 0, column = 1)
+
+    ent1 = Entry(f1)
+    ent2 = Entry(f1)
+
+    ent1.grid(row = 1, column = 1)
+    ent2.grid(row = 2, column = 1)
+
+    btn1 = Button(win1, text = '계산')
+    btn1.pack()
+
+    win1.mainloop()
+
 
 window=Tk()
 window.title('M  A  T  H')
@@ -56,7 +92,8 @@ b5= Button(window, text = "두 점 사이의 거리",
            fg = "#087666",
            width = 20, height = 2,
            bg = '#b4dede',
-           relief = SOLID)
+           relief = SOLID,
+           command = btn5)
 
 b6= Button(window, text = "연습문제",
            font = ("맑은 고딕", "10", 'bold'),
