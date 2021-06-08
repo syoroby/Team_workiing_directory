6982f746f59897dd5b0ced61ca33827aa5fdc215
galeb <83633279+changhyeok-an@users.noreply.github.com>
2021-06-08 18:03:19 +0900

    Update gittkinterexe.py

--- gittkinterexe.py
+++ gittkinterexe.py
@@ -1,11 +1,34 @@
 from tkinter import *
+import tkinter
+import tkinter as tk
 
-window=Tk()
+def btn1_button():
+    root1=tkinter.Tk()
+    root1.grab_set()
+    root1.title("이름")
+    label=tkinter.Label(root1, text="이름")
+    lbl = Label(root1, text = "이름")
+    lbl.grid(row = 0, column = 0)
+    txt1 = Entry(root1)
+    txt1.grid(row = 0, column = 1)
+    
+    lb2 = Label(root1, text = "이름")
+    lb2.grid(row = 1, column = 0)
+    txt2 = Entry(root1)
+    txt2.grid(row = 1, column = 1)
 
-w = Label(window, text="수학 보조용 프로그램 math입니다.")
-w.grid(row=0, column=3)
+    btn = tk.Button(root1, text = "Ok", width = 15)
+    btn.grid(row = 3, column = 1)
+  
+    root1.mainloop()
 
-b1=button = Button(window, text = "n차 함수 그래프")
+window=tkinter.Tk()
+
+label = tkinter.Label(window, text="수학 보조용 프로그램 math입니다.")
+label.grid()
+
+
+b1=tk.Button(window, text = "n차 함수 그래프",command=btn1_button)
 b1.grid(row=2, column=2)
 b2=button = Button(window, text = "피타고라스의 함수")
 b2.grid(row=3, column=2)
