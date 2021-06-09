fe7e48c31d19c5e24197a19dfa2806d238b7e388
jjPark <lucero23@naver.com>
2021-06-09 21:34:37 +0900

    Change to class

--- gittkinterexe.py
+++ gittkinterexe.py
@@ -1,41 +1,42 @@
 from tkinter import *
 
-def btn5():
-    win1 = Toplevel(window)
-    win1.configure(background = 'white')
+class Button5(Toplevel):
+    def __init__(self):
+        Toplevel.__init__(self)
+        
+        self.initialize()
+	
+    def initialize(self):
+        self.configure(background='white')
+        title = Label(self, text = "두 점 사이의 거리",
+                      font = ("맑은 고딕", "21", 'bold'),
+                      height=2,
+                      foreground = '#087666',
+                      background = 'white')
+        title.pack()
 
-    title = Label(win1, text = "두 점 사이의 거리",
-                  font = ("맑은 고딕", "21", 'bold'),
-                  height=2,
-                  foreground = '#087666',
-                  background = 'white')
-    title.pack()
+        f1 = Frame(self, bg = 'white')
+        f1.pack()
 
-    f1 = Frame(win1, bg = 'white')
-    f1.pack()
+        txt1 = Label(f1, text = "첫번째 점",
+                     background = 'white')
+        txt2 = Label(f1, text = "두번째 점",
+                     background = 'white')
+        txt3 = Label(f1, text = "좌표 (x, y)",
+                     background = 'white')
 
-    txt1 = Label(f1, text = "첫번째 점",
-                 background = 'white')
-    txt2 = Label(f1, text = "두번째 점",
-                 background = 'white')
-    txt3 = Label(f1, text = "좌표 (x, y)",
-                 background = 'white')
-
-    txt1.grid(row = 1, column = 0)
-    txt2.grid(row = 2, column = 0)
-    txt3.grid(row = 0, column = 1)
-
-    ent1 = Entry(f1)
-    ent2 = Entry(f1)
-
-    ent1.grid(row = 1, column = 1)
-    ent2.grid(row = 2, column = 1)
-
-    btn1 = Button(win1, text = '계산')
-    btn1.pack()
-
-    win1.mainloop()
-
+        txt1.grid(row = 1, column = 0)
+        txt2.grid(row = 2, column = 0)
+        txt3.grid(row = 0, column = 1)
+        
+        self.entryValue1 = StringVar()
+        self.entry1 = Entry(f1, textvariable = self.entryValue1)
+        
+        self.entryValue2 = StringVar()
+        self.entry2 = Entry(f1, textvariable = self.entryValue2)
+        
+        self.entry1.grid(row = 1, column = 1)
+        self.entry2.grid(row = 2, column = 1)
 
 window=Tk()
 window.title('M  A  T  H')
@@ -93,7 +94,7 @@ b5= Button(window, text = "두 점 사이의 거리",
            width = 20, height = 2,
            bg = '#b4dede',
            relief = SOLID,
-           command = btn5)
+           command = Button5)
 
 b6= Button(window, text = "연습문제",
            font = ("맑은 고딕", "10", 'bold'),
