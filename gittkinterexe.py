51205be41ee4bb1f96932de0cbc979f672b59930
jjPark <lucero23@naver.com>
2021-06-09 23:00:17 +0900

    Button 5

--- gittkinterexe.py
+++ gittkinterexe.py
@@ -1,42 +1,59 @@
 from tkinter import *
+import point
 
 class Button5(Toplevel):
-    def __init__(self):
-        Toplevel.__init__(self)
-        
-        self.initialize()
-	
-    def initialize(self):
-        self.configure(background='white')
-        title = Label(self, text = "두 점 사이의 거리",
-                      font = ("맑은 고딕", "21", 'bold'),
-                      height=2,
-                      foreground = '#087666',
-                      background = 'white')
-        title.pack()
+        def __init__(self):
+                Toplevel.__init__(self)
 
-        f1 = Frame(self, bg = 'white')
-        f1.pack()
+                self.initialize()
 
-        txt1 = Label(f1, text = "첫번째 점",
-                     background = 'white')
-        txt2 = Label(f1, text = "두번째 점",
-                     background = 'white')
-        txt3 = Label(f1, text = "좌표 (x, y)",
-                     background = 'white')
+        def initialize(self):
+                self.configure(background='white')
+                title = Label(self, text = "두 점 사이의 거리",
+                              font = ("맑은 고딕", "21", 'bold'),
+                              height=2,
+                              foreground = '#087666',
+                              background = 'white')
+                title.pack()
 
-        txt1.grid(row = 1, column = 0)
-        txt2.grid(row = 2, column = 0)
-        txt3.grid(row = 0, column = 1)
-        
-        self.entryValue1 = StringVar()
-        self.entry1 = Entry(f1, textvariable = self.entryValue1)
-        
-        self.entryValue2 = StringVar()
-        self.entry2 = Entry(f1, textvariable = self.entryValue2)
-        
-        self.entry1.grid(row = 1, column = 1)
-        self.entry2.grid(row = 2, column = 1)
+                f1 = Frame(self, bg = 'white')
+                f1.pack()
+
+                txt1 = Label(f1, text = "첫번째 점",
+                             background = 'white')
+                txt2 = Label(f1, text = "두번째 점",
+                             background = 'white')
+                txt3 = Label(f1, text = "좌표 (x, y)",
+                             background = 'white')
+
+                txt1.grid(row = 1, column = 0)
+                txt2.grid(row = 2, column = 0)
+                txt3.grid(row = 0, column = 1)
+
+                self.entryValue1 = StringVar()
+                self.entry1 = Entry(f1, textvariable = self.entryValue1)
+
+                self.entryValue2 = StringVar()
+                self.entry2 = Entry(f1, textvariable = self.entryValue2)
+
+                self.entry1.grid(row = 1, column = 1)
+                self.entry2.grid(row = 2, column = 1)
+
+                btn = Button(self, text = "계산", command = self.Calculate)
+                btn.pack()
+
+                self.resultValue = StringVar()
+                self.result = Label(self, bg = 'white',
+                                    textvariable = self.resultValue)
+                self.result.pack()
+
+        def Calculate(self):
+                x1, y1 = point.get_float(self.entryValue1.get())
+                x2, y2 = point.get_float(self.entryValue2.get())
+
+                result = "두 점 사이의 거리는 %.2f입니다"%point.point_len(x1, x2, y1, y2)
+                self.resultValue.set(result)
+                
 
 class MainPage(Tk):
         def __init__(self):
@@ -119,4 +136,3 @@ class MainPage(Tk):
 
 window = MainPage()
 window.mainloop()
-
